a436f455f246548e5f262562e65dd61bb7ba3c8b
Mark Stover <stovenator@gmail.com>
2016-11-04 17:55:38 -0500

    Switch to path instead of host for different apps

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ var express = require('express');
 var proxy = require('express-http-proxy');
 var device = require('express-device');
 var moment = require('moment');
+var url = require('url');
 
 var app = express();
 var start = moment().format('hh:m:ss');
 
 
-var betaServerName = process.env.CM_ACCEPT_BETA_SERVER || 'localhost:9090';
-var mainServerName = process.env.CM_ACCEPT_SERVER || 'localhost:9090';
+var betaServer = process.env.CM_ACCEPT_BETA_SERVER || 'localhost:9090';
+var mainServer = process.env.CM_ACCEPT_MAIN_SERVER || 'localhost:9091';
+var betaServerPath = process.env.CM_ACCEPT_BETA_PATH || '';
+var mainServerPath = process.env.CM_ACCEPT_MAIN_PATH || '';
 
-var cmAcceptBetaServer = proxy(betaServerName);
-var cmAcceptServer = proxy(mainServerName);
+var cmAcceptBetaServer = proxy(betaServer, {
+    forwardPath: function(req, res) {
+        return betaServerPath + url.parse(req.url).path;
+    }
+});
+var cmAcceptServer = proxy(mainServer, {
+    forwardPath: function(req, res) {
+        return mainServerPath + url.parse(req.url).path;
+    }
+});
 
 app.set('port', process.argv[2] || process.env.PORT || 8801);
 
@@ -28,11 +39,9 @@ app.use('/diagnostic', function(req,res,next){
 
 app.use('/', function(req, res, next){
     if (req.device.type == 'phone'){
-        console.log("Requesting ", req.originalUrl, " from server : ", mainServerName);
         cmAcceptServer(req, res, next);
     }
     else{
-        console.log("Requesting ", req.originalUrl, " from server : ", betaServerName);
         cmAcceptBetaServer(req, res, next);
     }
 });
